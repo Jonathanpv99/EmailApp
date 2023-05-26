854e423f9f11f879e485be483a13d7677d3b59a4
Jonathanpv99 <jonyvegapjv@gmail.com>
2023-05-25 18:15:14 -0600

    comite

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { EmailComposer } from '@awesome-cordova-plugins/email-composer/ngx';
+import { EmailComposerOptions } from '@awesome-cordova-plugins/email-composer/ngx';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,39 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  hasAccount = false;
+  currentImage?= "";
+  imageData? = "";
 
-  constructor() {}
+
+  constructor(private emailComposer: EmailComposer) {}
+
+  async checkAccount(){
+    this.hasAccount = await this.emailComposer.hasAccount();
+  }
+
+  async captureImage(){
+    const image= await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Camera
+    });
+    this.imageData = image.base64String;
+    this.currentImage = `data:image/jpg;base64,${image.base64String}`;
+  }
+
+  async openEmail(){
+    const email:  EmailComposerOptions = {
+      to: 'jonyvegapjv@gmail.com',
+      cc: 's18120160@alumnos.itsur.edu.mx',
+      attachments: [`base64:image.jpg,$this.imageData`],
+      subject: 'imagen chida',
+      body: 'tire paro profe pasenos la materia :C',
+      isHtml: true
+    };
+
+    await this.emailComposer.open(email);
+  }
 
 }
